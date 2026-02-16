b988bdf86560217eafd0e072ce1eb01c770951d7
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-16 17:06:09 -0300

    Add Koin initialization to iOS MainViewController for dependency injection setup

--- composeApp/src/iosMain/kotlin/com/location_tracker/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/location_tracker/MainViewController.kt
@@ -1,5 +1,10 @@
 package com.location_tracker
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.location_tracker.di.KoinInitializer
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() =
+    ComposeUIViewController {
+        KoinInitializer.setupKoin()
+        App()
+    }
