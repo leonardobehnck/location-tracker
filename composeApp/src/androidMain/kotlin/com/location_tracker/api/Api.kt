fcc2409710b2682caae24340afb2798e505768dc
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-16 15:56:24 -0300

    Refactor: Remove Api interface and ApiImpl implementation

--- composeApp/src/androidMain/kotlin/com/location_tracker/api/Api.kt
+++
@@ -1,7 +0,0 @@
-package com.location_tracker.api
-
-import com.location_tracker.data.remote.dto.LocationTrackingRequestDto
-
-interface Api {
-    suspend fun sendLocationTracking(request: LocationTrackingRequestDto): Response<Unit>
-}
