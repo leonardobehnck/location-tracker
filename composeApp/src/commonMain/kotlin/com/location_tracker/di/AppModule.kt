f2d28adabe9c6be7cd49ec93d2f1deefa7622b2f
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-16 15:58:12 -0300

    Refactor Koin module setup for iOS and common code

--- composeApp/src/commonMain/kotlin/com/location_tracker/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/location_tracker/di/AppModule.kt
@@ -3,11 +3,13 @@ package com.location_tracker.di
 import com.location_tracker.RequestContentHolder
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import org.koin.core.module.Module
 
 internal expect fun platformAppModule(): Module
 
-fun appModule(): Module =
-    module {
-        single { RequestContentHolder() }
-    } + platformAppModule()
+fun appModule(): List<Module> =
+    listOf(
+        module {
+            single { RequestContentHolder() }
+        },
+        platformAppModule(),
+    )
--- composeApp/src/iosMain/kotlin/com/location_tracker/di/AppModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/location_tracker/di/AppModule.ios.kt
@@ -1,9 +1,20 @@
 package com.location_tracker.di
 
-import com.location_tracker.location.LocationTrackingManager
+import com.location_tracker.api.Api
+import com.location_tracker.api.ApiImpl
+import com.location_tracker.data.cache.InMemoryLocationCacheIos
+import com.location_tracker.data.cache.LocationCache
+import com.location_tracker.data.network.IosNetworkAvailability
+import com.location_tracker.data.network.NetworkAvailability
+import com.location_tracker.models.LocationTrackingRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-internal actual fun platformAppModule() = module {
-    singleOf(::LocationTrackingManager)
-}
+internal actual fun platformAppModule() =
+    module {
+        single { ApiImpl(provideHttpClientEngine()) }
+        single<Api> { get<ApiImpl>() }
+        single<LocationCache> { InMemoryLocationCacheIos() }
+        single<NetworkAvailability> { IosNetworkAvailability() }
+        singleOf(::LocationTrackingRepository)
+    }
